63a6b97d427e3fb5b83c01592ea5ba9b410c6e8e
Tony <tonydgoodwin@gmail.com>
2025-01-13 19:46:23 -0500

    vhdl 2019 test

--- hdl/bd/led_cnt_wrapper.v
+++ hdl/bd/led_cnt_wrapper.v
@@ -16,9 +16,18 @@ module led_cnt_wrapper (
     .wren_i (1'b0     ),
     .led_o  (led_o    )
   );
-*/
+
 
   led_cnt_vhd08 led_cnt_vhd08_inst(
+    .rst        (rst    ),
+    .clk        (clk100 ),
+    .div_i      (div_i  ),
+    .wren_i     (1'b0   ),
+    .led_o      (led_o  )
+  );
+*/
+
+  led_cnt_vhd19 led_cnt_vhd19_inst(
     .rst        (rst    ),
     .clk        (clk100 ),
     .div_i      (div_i  ),
