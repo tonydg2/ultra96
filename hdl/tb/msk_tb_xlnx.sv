cef177515fd399f343349db21e83f9ccd9d0d8db
Tony <tonydgoodwin@gmail.com>
2025-03-03 15:44:24 -0500

    automated commit & push

--- hdl/tb/msk_tb_xlnx.sv
+++ hdl/tb/msk_tb_xlnx.sv
@@ -134,7 +134,7 @@ module msk_tb;
     ) msk_demod_inst (
         .clk(clk),
         .reset_n(reset_n),
-        .midpoint_adj(-9),
+        .midpoint_adj(-3),
         .i_in(fir_I_tdata[30:15]),
         .q_in(fir_Q_tdata[30:15]),
         .data_out()
